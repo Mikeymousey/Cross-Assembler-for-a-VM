350058cf676dd35d452f888560f9472e05e19acc
HWJFish <wwc641810324@gmail.com>
2020-11-15 18:49:18 -0500

    added all instructions for sprint 1

--- Cross-Assembler for Virtual Machine/src/assembler/SymbolTable.java
+++ Cross-Assembler for Virtual Machine/src/assembler/SymbolTable.java
@@ -1,7 +1,7 @@
 package assembler;
 
 public class SymbolTable {
-	Node[] table = new Node[11];
+	Node[] table = new Node[25];
 	public SymbolTable() {
 		table[0] = new Node("halt", 0);
 		table[1] = new Node("pop", 1);
@@ -14,6 +14,20 @@ public class SymbolTable {
 		table[8] = new Node("xor", 15);
 		table[9] = new Node("neg", 16);
 		table[10] = new Node("inc", 17);
+		table[11] = new Node("dec", 18);
+		table[12] = new Node("add", 19);
+		table[13] = new Node("sub", 20);
+		table[14] = new Node("mul", 21);
+		table[15] = new Node("div", 22);
+		table[16] = new Node("rem", 23);
+		table[17] = new Node("shl", 24);
+		table[18] = new Node("shr", 25);
+		table[19] = new Node("teq", 26);
+		table[20] = new Node("tne", 27);
+		table[21] = new Node("tlt", 28);
+		table[22] = new Node("tgt", 29);
+		table[23] = new Node("tle", 30);
+		table[24] = new Node("tge", 31);
 		
 	}
 	public void convertToLst(Linestatement linestatement) {
